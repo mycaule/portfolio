95acdc91dcd2284a090e9f26d7e16d4dd473d3cf
Michel <mycaule@users.noreply.github.com>
2018-10-14 21:24:59 +0200

    Update gdax.test.js

--- services/gdax.test.js
+++ services/gdax.test.js
@@ -8,7 +8,7 @@ global.WebSocket = WebSocket
 test('products', async t => {
   const result = await gdax.products()
   const productIds = result.map(_ => _.id)
-  t.deepEqual(productIds.sort(), ['BCH-BTC', 'BCH-EUR', 'BCH-GBP', 'BCH-USD', 'BTC-EUR', 'BTC-GBP', 'BTC-USD', 'ETC-BTC', 'ETC-EUR', 'ETC-GBP', 'ETC-USD', 'ETH-BTC', 'ETH-EUR', 'ETH-GBP', 'ETH-USD', 'LTC-BTC', 'LTC-EUR', 'LTC-USD', 'ZRX-BTC', 'ZRX-EUR', 'ZRX-USD'])
+  t.deepEqual(productIds.sort(), ['BCH-BTC', 'BCH-EUR', 'BCH-GBP', 'BCH-USD', 'BTC-EUR', 'BTC-GBP', 'BTC-USD', 'ETC-BTC', 'ETC-EUR', 'ETC-GBP', 'ETC-USD', 'ETH-BTC', 'ETH-EUR', 'ETH-GBP', 'ETH-USD', 'LTC-BTC', 'LTC-EUR', 'LTC-GBP', 'LTC-USD', 'ZRX-BTC', 'ZRX-EUR', 'ZRX-USD'])
 })
 
 test('candles', async t => {
